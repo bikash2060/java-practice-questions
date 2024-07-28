928bf0e1532e92c6041b9cb8b3a4056c446fbae6
bikash2060 <bkashbhattarai@gmail.com>
2024-07-28 15:31:55 +0545

    Concept of methods

--- src/OOP/Methods/StaticMethod.java
+++ src/OOP/Methods/StaticMethod.java
@@ -2,11 +2,14 @@ package OOP.Methods;
 
 public class StaticMethod {
 
+    //To create static method: Use 'static' keyword right after access modifier
     public static boolean verifyAge(int age) {
         return age >= 18;
     }
 
     public static void main(String[] args) {
+
+        //No need to mention class name if we are calling static method in the same class
         if (verifyAge(18)) {
             System.out.println("You are eligible for voting");
         }
