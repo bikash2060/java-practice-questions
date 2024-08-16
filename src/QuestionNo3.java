96edf4d5940e3cab9e35d84a84b9511deb17b57f
bikash2060 <bkashbhattarai@gmail.com>
2024-08-16 21:56:31 +0545

    Extra questions added

---
+++ src/QuestionNo3.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList; // Import the ArrayList class from the util package
+import java.util.Iterator; // Import the Iterator interface from the util package
+import java.util.List; // Import the List interface from the util package
+
+public class QuestionNo3 { // Define a public class named QuestionNo3
+
+    public static void main(String[] args) { // Main method to execute the program
+
+        Iterator<String> newIterator = List.of("1stelement", "2ndelement", "3rdelement").iterator(); // Create an Iterator from a list with three elements
+
+        List<String> convertedList = convertIteratorToList(newIterator); // Convert the Iterator to a List using the convertIteratorToList method
+
+        System.out.println(convertedList); // Print the converted list to the console
+
+    }
+    private static List<String> convertIteratorToList(Iterator<String> iterator) { // Method to convert an Iterator to a list
+
+        List<String> emptyList = new ArrayList<>(); // Create an empty ArrayList to hold the elements
+        while (iterator.hasNext()) { // Iterate through the Iterator and add each element to the List
+            emptyList.add(iterator.next());
+        }
+        return emptyList; // Return the converted list
+    }
+}
